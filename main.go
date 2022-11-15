c2b6eaed113186b5c289892b2d4e47884019f5b9
LinkinPeck <pexbox@hotmail.com>
2022-11-15 15:22:26 +0700

    add exname

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 var y int
 
+var exname string = "peck"
+
 func main() {
 	cal(sub)
 }
 
 func cal(f func(int, int) int) {
 	sum := f(50, 10)
-	println(sum)
+	println(sum, exname)
 }
 
 func add(a, b int) int {
