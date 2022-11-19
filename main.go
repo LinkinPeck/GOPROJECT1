4a72b16a1c1f315419e4daf4cae5f7d5389c22ce
LinkinPeck <pexbox@hotmail.com>
2022-11-19 23:34:13 +0700

    Start kong version

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
-var y int
-
-var exname string = "peck"
+import "fmt"
 
 func main() {
-	cal(sub)
-}
-
-func cal(f func(int, int) int) {
-	sum := f(50, 10)
-	println(sum, exname)
-}
-
-func add(a, b int) int {
-	return a + b
-}
-func sub(a, b int) int {
-	return a - b
+	fmt.Println("Hello Peck")
+	var x int
+	x = 20
+	fmt.Println(x)
 }
