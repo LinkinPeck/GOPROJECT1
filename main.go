c9f422300c84c5fdbf6fdd557398273e83853585
LinkinPeck <pexbox@hotmail.com>
2022-11-21 14:35:49 +0700

    Scanf

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "John"
-	var score float64 = 5555.5
-	fmt.Println("Hello", name)
-	var x int = 50
-
-	fmt.Println(x, name, score, &x, &name, &score)
+	var x int
+	fmt.Print("Answer me  = ")
+	fmt.Scanf("%d", x)
+	fmt.Println("Hi ", x)
 }
